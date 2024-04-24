c46fb2110c5e150f677e1f7abc1963937c974346
bxbyguts <157765240+bxbyguts@users.noreply.github.com>
2024-04-24 22:05:32 +0000

    revert

--- script.js
+++
@@ -1,5 +0,0 @@
-// Check if the screen width is below a certain threshold (e.g., 768 pixels)
-if (window.innerWidth < 768) {
-    // Redirect to the mobile index page
-    window.location.href = "index_mobile.html";
-}
